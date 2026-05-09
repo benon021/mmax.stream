860038a12461d91a4a2dab605691550337583dd3
benon021 <papsiiseme@gmail.com>
2026-05-09 17:54:25 +0300

    Add sandbox to iframe to block ads

--- src/components/MovieModal.jsx
+++ src/components/MovieModal.jsx
@@ -286,6 +286,7 @@ function MovieModal({ movie, onClose }) {
                   allowFullScreen
                   frameBorder="0"
                   onLoad={() => setIsVideoLoading(false)}
+                  sandbox="allow-scripts allow-same-origin allow-forms"
                 ></iframe>
               </div>
 
